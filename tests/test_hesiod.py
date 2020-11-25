5dd821770c8fdd66595a7839b41e21b48a99042b
lykius <lucadeluigi91@gmail.com>
2020-11-25 16:11:58 +0100

    chore: clean hesiod tests

--- tests/test_hesiod.py
+++ tests/test_hesiod.py
@@ -9,11 +9,27 @@ def test_version() -> None:
     assert hesiod.__version__ == "0.1.0"
 
 
+def test_args_kwargs() -> None:
+    cwd = Path(".").absolute()
+    run_cfg_file = cwd / "tests/runs/run_simple.yaml"
+    base_cfg_dir = cwd / "tests/cfg"
+
+    @hesiod.main(base_cfg_dir, run_cfg_file)
+    def test(a: int, b: str, c: float = 3.4) -> Tuple[int, str, float]:
+        return a, b, c
+
+    ra, rb, rc = test(2, "param_b", c=1.23456)
+    assert ra == 2
+    assert rb == "param_b"
+    assert rc == 1.23456
+
+
 def test_load_config() -> None:
     cwd = Path(".").absolute()
-    cfg_path = cwd / "tests/run_simple.yaml"
+    run_cfg_file = cwd / "tests/runs/run_simple.yaml"
+    base_cfg_dir = cwd / "tests/cfg"
 
-    @hesiod.main(cfg_path.parent, cfg_path)
+    @hesiod.main(base_cfg_dir, run_cfg_file)
     def test() -> None:
         assert hesiod.get_param("group_1.param_a") == 1
         assert hesiod.get_param("group_1.param_b") == 1.2
@@ -28,9 +44,10 @@ def test_load_config() -> None:
 
 def test_get_param() -> None:
     cwd = Path(".").absolute()
-    cfg_path = cwd / "tests/run_simple.yaml"
+    run_cfg_file = cwd / "tests/runs/run_simple.yaml"
+    base_cfg_dir = cwd / "tests/cfg"
 
-    @hesiod.main(cfg_path.parent, cfg_path)
+    @hesiod.main(base_cfg_dir, run_cfg_file)
     def test() -> None:
         g1pa = hesiod.get_param("group_1.param_a", int)
         assert g1pa == 1 and isinstance(g1pa, int)
@@ -54,17 +71,3 @@ def test_get_param() -> None:
             hesiod.get_param("group_2.param_d", bool)
 
     test()
-
-
-def test_args_kwargs() -> None:
-    cwd = Path(".").absolute()
-    cfg_path = cwd / "tests/run_simple.yaml"
-
-    @hesiod.main(cfg_path.parent, cfg_path)
-    def test(a: int, b: str, c: float = 3.4) -> Tuple[int, str, float]:
-        return a, b, c
-
-    ra, rb, rc = test(2, "param_b", c=1.23456)
-    assert ra == 2
-    assert rb == "param_b"
-    assert rc == 1.23456
