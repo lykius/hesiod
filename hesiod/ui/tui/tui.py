b9454edcbe5b2aacedb0073a377cfb26d9eedd47
Luca De Luigi <lucadeluigi91@gmail.com>
2020-12-03 18:18:07 +0100

    refactor: pass cfg parser to UI

--- hesiod/ui/tui/tui.py
+++ hesiod/ui/tui/tui.py
@@ -1,22 +1,26 @@
 from pathlib import Path
+from typing import Type
 
 from npyscreen import NPSAppManaged  # type: ignore
 
-from hesiod.cfgparse import CFG_T
+from hesiod.cfgparse import CFG_T, ConfigParser
 from hesiod.ui.tui.editform import EditForm
 from hesiod.ui.tui.recapform import RecapForm
 from hesiod.ui.ui import UI
 
 
 class TUI(UI, NPSAppManaged):
-    def __init__(self, template_cfg: CFG_T, base_cfg_dir: Path) -> None:
+    def __init__(
+        self, template_cfg: CFG_T, base_cfg_dir: Path, cfgparser: Type[ConfigParser]
+    ) -> None:
         """Create a new terminal user interface (TUI).
 
         Args:
             template_file: path to the config template file.
             base_cfg_dir: path to the base configs directory.
+            cfgparser: config parser.
         """
-        UI.__init__(self, template_cfg, base_cfg_dir)
+        UI.__init__(self, template_cfg, base_cfg_dir, cfgparser)
         NPSAppManaged.__init__(self)
         self.run_cfg: CFG_T = {}
 
