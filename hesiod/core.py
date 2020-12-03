56a00a241d80fcd318c40662e63478c2ed5d857e
Luca De Luigi <lucadeluigi91@gmail.com>
2020-12-03 18:19:47 +0100

    feat: save out dir path in global config

--- hesiod/core.py
+++ hesiod/core.py
@@ -10,6 +10,7 @@ T = TypeVar("T")
 FUNCTION_T = Callable[..., Any]
 _CFG: CFG_T = {}
 RUN_FILE_NAME = "run.yaml"
+OUT_DIR_KEY = "***hesiod_out_dir***"
 
 
 def hmain(
@@ -17,6 +18,7 @@ def hmain(
     template_cfg_file: Optional[Path] = None,
     run_cfg_file: Optional[Path] = None,
     create_out_dir: bool = True,
+    out_dir_root: str = "logs",
 ) -> Callable[[FUNCTION_T], FUNCTION_T]:
     """Decorator for a given function.
 
@@ -28,6 +30,11 @@ def hmain(
         template_cfg_file: the path to the template config file for this run (optional).
         run_cfg_file: the path to the config file created by the user for this run (optional).
         create_out_dir: flag that indicates whether hesiod should create an output directory or not.
+        out_dir_root: root for output directories.
+
+    Raises:
+        ValueError: if both template_cfg_file and run_cfg_file are None.
+        ValueError: if the run name is not specified in the run file.
 
     Returns:
         Function wrapped in the decorator.
@@ -48,7 +55,7 @@ def hmain(
             elif template_cfg_file is not None:
                 parser = get_parser(template_cfg_file.suffix)
                 template_cfg = parser.load_cfg(template_cfg_file, base_cfg_dir)
-                tui = TUI(template_cfg, base_cfg_dir)
+                tui = TUI(template_cfg, base_cfg_dir, parser)
                 _CFG = tui.show()
 
             if create_out_dir:
@@ -57,8 +64,9 @@ def hmain(
                     msg = f"The run file must contain a valid name for the run ({RUN_NAME_KEY})."
                     raise ValueError(msg)
 
-                run_dir = Path(run_name)
+                run_dir = Path(out_dir_root) / Path(run_name)
                 run_dir.mkdir(parents=True, exist_ok=False)
+                _CFG[OUT_DIR_KEY] = run_dir
                 run_file = run_dir / RUN_FILE_NAME
                 parser = get_parser(run_file.suffix)
                 parser.save_cfg(_CFG, run_file)
@@ -71,14 +79,14 @@ def hmain(
 
 
 def hcfg(name: str, t: Optional[Type[T]] = None) -> T:
-    """Get requested parameter from global configuration.
+    """Get the requested parameter from global configuration.
 
     Args:
         name: name of the parameter.
         t: type of the parameter.
 
     Raises:
-        ValueError: if the requested parameter is not of the required type.
+        TypeError: if the requested parameter is not of the required type.
 
     Returns:
         The requested parameter.
@@ -86,8 +94,12 @@ def hcfg(name: str, t: Optional[Type[T]] = None) -> T:
     value = _CFG
     for n in name.split("."):
         value = value[n]
+
     if t is not None and not isinstance(value, t):
-        raise ValueError(f"{name} is of type {type(value)} but requested {t}")
+        raise TypeError(f"{name} is of type {type(value)} but requested {t}")
+
+    value = deepcopy(value)
+
     return cast(T, value)
 
 
@@ -98,3 +110,12 @@ def get_cfg_copy() -> CFG_T:
         A copy of the global configuration.
     """
     return deepcopy(_CFG)
+
+
+def get_out_dir() -> Path:
+    """Get path to the output directory for the current run.
+
+    Returns:
+        Path to the output directory.
+    """
+    return deepcopy(_CFG[OUT_DIR_KEY])
