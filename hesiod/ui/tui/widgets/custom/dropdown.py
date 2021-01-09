6debd05a9691bbcd5440115ee28ee5faa33c1cec
Luca De Luigi <lucadeluigi91@gmail.com>
2021-01-09 14:25:52 +0100

    fix: set focus on parent widget when closing popup

--- hesiod/ui/tui/widgets/custom/dropdown.py
+++ hesiod/ui/tui/widgets/custom/dropdown.py
@@ -1,4 +1,15 @@
-from asciimatics.widgets import DropdownList  # type: ignore
+from typing import Optional
+
+from asciimatics.event import Event, KeyboardEvent, MouseEvent  # type: ignore
+from asciimatics.screen import Screen  # type: ignore
+from asciimatics.widgets import DropdownList, _DropdownPopup  # type: ignore
+
+
+class CustomDropdownPopup(_DropdownPopup):
+    def _on_close(self, cancelled: bool) -> None:
+        _DropdownPopup._on_close(self, cancelled)
+        if not cancelled:
+            self._parent.focus()
 
 
 class CustomDropdownList(DropdownList):
@@ -7,3 +18,18 @@ class CustomDropdownList(DropdownList):
         text = "" if self._line is None else self._options[self._line][0]
         (colour, attr, background) = self._pick_colours("field", selected=self._has_focus)
         self._frame.canvas.print_at(text, self._x + self._offset, self._y, colour, attr, background)
+
+    def process_event(self, event: Optional[Event]) -> Optional[Event]:
+        if event is not None:
+            if isinstance(event, KeyboardEvent):
+                if event.key_code in [Screen.ctrl("M"), Screen.ctrl("J"), ord(" ")]:
+                    event = None
+            elif isinstance(event, MouseEvent):
+                if event.buttons != 0:
+                    if self.is_mouse_over(event, include_label=False):
+                        event = None
+            if event is None:
+                self._child = CustomDropdownPopup(self)
+                self.frame.scene.add_effect(self._child)
+
+        return event
