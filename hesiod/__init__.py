2a8237a5123fdb969cb22ec8e8f1ac9bb8e9a877
Luca De Luigi <lucadeluigi91@gmail.com>
2021-01-12 21:54:35 +0100

    feat: create version 0.2.1

--- hesiod/__init__.py
+++ hesiod/__init__.py
@@ -1,4 +1,4 @@
 from hesiod.core import get_cfg_copy, get_out_dir, get_run_name, hcfg, hmain
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = ["__version__", "hmain", "hcfg", "get_cfg_copy", "get_out_dir", "get_run_name"]
