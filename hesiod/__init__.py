28213026f5a03ebc34c881bf7c53571701683de2
Luca De Luigi <lucadeluigi91@gmail.com>
2021-02-06 11:39:26 +0100

    chore: remove old function

--- hesiod/__init__.py
+++ hesiod/__init__.py
@@ -3,15 +3,7 @@ from pkg_resources import DistributionNotFound
 
 from hesiod.core import get_cfg_copy, get_out_dir, get_run_name, hcfg, hmain
 
-__all__ = [
-    "__version__",
-    "hmain",
-    "hcfg",
-    "get_cfg_copy",
-    "get_out_dir",
-    "get_run_name",
-    "parse_args",
-]
+__all__ = ["__version__", "hmain", "hcfg", "get_cfg_copy", "get_out_dir", "get_run_name"]
 
 try:
     __version__ = pkg_resources.get_distribution("hesiod").version
